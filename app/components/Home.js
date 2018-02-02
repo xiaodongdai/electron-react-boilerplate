b9c3826e450b9612384bb4131136e81ee54dab7f
Ken Dai <ken.dai@outlook.com>
2018-02-02 22:52:40 +0100

    try a little material UI library

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,3 +1,4 @@
+"use strict"
 // @flow
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
@@ -5,6 +6,7 @@ import styles from './Home.css'
 import Parser from 'html-react-parser'
 import SplitPane from 'react-split-pane'
 import PrimaryPane from '../containers/PrimaryPane'
+import TextField from 'material-ui/TextField';
 
 type Props = {
   queryAsync: () => void,
@@ -19,6 +21,11 @@ export default class Home extends Component<Props> {
     this.handleClickItem = this.handleClickItem.bind(this)
     this.handleSort = this.handleSort.bind(this)
     this.handleReview = this.handleReview.bind(this)
+    this._handleTextChange = this._handleTextChange.bind(this)
+  }
+
+  _handleTextChange(e) {
+    this._queryWord = e.target.value
   }
 
   handleSort() {
@@ -29,13 +36,17 @@ export default class Home extends Component<Props> {
   }
 
   handleOnClickAsync() {
-    const {_word} = this.refs
-    console.log(`value=${_word.value}`)
+    //const {_word} = this.refs
+    console.log('this: ' , this)
+    console.log('value is:', this._queryWord)
+    if (!this._queryWord || this._queryWord === '') {
+      return
+    }
     let {
       queryAsync
     } = this.props;
     console.log(this.props)
-    queryAsync(_word.value)
+    queryAsync(this._queryWord)
   }
 
   handleClickItem(word) {
@@ -63,17 +74,7 @@ export default class Home extends Component<Props> {
   render() {
     let {explain, rankInfo, wordList, curState} = this.props
     // reviewData: curWord, wordLists, 
-    let reviewPane = (reviewData) => {
-      return (<div>
-            {reviewData.curWord}
-            Do you know?
-            <br/>
-            <button type="button" onClick={this.handleKnow}>I Know</button>
-            <button type="button" onClick={this.handleDontKnow}>Not Know</button>
-            </div>)
-    }
 
-    let Explain = props => Parser(explain)
     
     let WordList = () => {
       let items = wordList.map((item, idx) => 
@@ -84,11 +85,10 @@ export default class Home extends Component<Props> {
       return (<ul>{items}</ul>)
     }
 
- 
     return (
       <SplitPane split="horizontal" allowResize={false} minSize={50} defaultSize={80}>
         <div className={styles.container} data-tid="container">
-          <input ref="_word" type="text" />
+          <TextField  label="Word" margin="normal" onChange={this._handleTextChange}/>
           <button type="button" onClick={this.handleOnClickAsync}>Query</button>
           <button type="button" onClick={this.handleAdd}>Add</button>
           <button type="button" onClick={this.handleReview}>Review</button>
