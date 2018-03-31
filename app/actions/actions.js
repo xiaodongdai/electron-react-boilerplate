e2be4b6c91f916929647376602547d5b2f714087
kendai <ken.dai@outlook.com>
2018-04-01 00:50:47 +0200

    working on multiple dictionary

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -184,87 +184,129 @@ function lookupRes(mdd, file) {
 }
 */
 
+
+
+function changeFontColor([originExplain]) {
+  console.log('changeFontColor ' + originExplain)
+  let explain = originExplain.replace('<font color="silver">', '<font color="black">')
+  return explain
+}
+
+
+// get external files from the explain
+function getExtFiles(explain) {
+  let re = /"(sound|file):\/\/([a-z0-9.]*)"/g
+  let matches = null
+  let queries = []
+  let files = []
+  console.log('explain:    ', explain)
+  console.log('match starting     ')
+  while (matches = re.exec(explain)) {
+    // the uri is like "sound://test.wav"
+    console.log(`matched1: ${matches[2]} and ${matches[0]}`)
+    files.push({filename: matches[2], originUri: matches[0]})
+  }
+
+  re = /<img +src *= *"([a-z0-9_]+\.[a-z]+)"/g
+  while (matches = re.exec(explain)) {
+    console.log(`matched2: ${matches[1]} and ${matches[1]}`)
+    files.push({filename: matches[1], originUri: matches[1]})
+  }
+  console.log('files:  ', files)
+  return files
+}
+
+
+
+
+function lookupMdd(mdd, files) {
+  return Promise.all(files.map(file => mdd.lookup(file.filename)))
+}
+
+
+function loadMdd(filename) {
+  return mdict.loadmdd(filename + '.mdd')
+}
+
+function replaceExplain(raws, explain, files) {
+  console.log('replaceExplain')
+
+  console.log(raws)
+  raws.forEach((raw, index) => {
+    let file = files[index]
+    let type = ''
+    let suffix = file.filename.substr(file.filename.length - 4)
+    switch(suffix) {
+      case '.spx':
+        let data = String.fromCharCode.apply(null, raw)
+        file.buffer = decodeFile(data)
+        console.log('buffer:      ', file.buffer )
+        type = 'audio/wav'
+        break
+      case '.mp3':
+        file.buffer = raw
+        type = 'audio/mp3'
+        break
+      case '.bmp':
+        file.buffer = raw
+        type = 'image/bmp'
+        break
+      case '.png':
+        file.buffer = raw
+        type = 'image/png'
+        break
+      case '.jpg':
+        file.buffer = raw
+        type = 'image/jpg'
+        break
+    }
+    let blob = new Blob([file.buffer], {type})
+    file.objectUri = URL.createObjectURL(blob)
+    explain = explain.replace(file.originUri, file.objectUri)
+  })
+
+  return {explain, files}
+}
+
+export function lookUpDictionary(word, filename, isMddExists = true) {
+
+  if (isMddExists) {
+    let localExplain = ''
+    let localFiles = []
+    return mdict.dictionary(filename + '.mdx').then(dictionary => dictionary.lookup(word))
+      .then(changeFontColor).then(explain => localExplain = explain)
+      .then(getExtFiles).then(files => {localFiles = files})
+      .then(() => loadMdd(filename))
+      .then(mdd => lookupMdd(mdd, localFiles))
+      .then(results => replaceExplain(results, localExplain, localFiles))
+  } else {
+    return mdict.dictionary(filename + '.mdx').then(dictionary => dictionary.lookup(word))
+      .then(changeFontColor)
+  }
+}
+
+
 export function queryAsync(word: string, review: bool = false) {
+  let lookup = lookUpDictionary
   return (dispatch: (action: actionType) => void) => {
-    mdict.dictionary(['dictionary.mdx']).then(dictionary => {
-      //// dictionary is loaded
-      dictionary.search({
-        phrase: word,      /// '*' and '?' supported
-        max: 10           /// maximum results
-      }).then(function(foundWords){
-        var word = ''+foundWords[0];
-        return dictionary.lookup(word); /// typeof word === string
-      }).then(([originExplain]) => {
-        let files = []
-        // parse the explain, find out the files or images.
-        mdict.loadmdd('dictionary.mdd').then(mdd => {
 
-          // download resources.
-          let re = /"(sound|file):\/\/([a-z0-9.]*)"/g
-          let matches = null
-          let queries = []
-          console.log('explain:  ', originExplain)
-          console.log('match starting ')
-          while (matches = re.exec(originExplain)) {
-            // the uri is like "sound://test.wav"
-            console.log(`matched1: ${matches[2]} and ${matches[0]}`)
-            files.push({filename: matches[2], originUri: matches[0]})
-          }
+    // TODO: multiple dictionary queries.
+    let a = lookUpDictionary(word, 'sv-en-folkets-lexikon', true)
+    console.log('a= ', a)
 
-          re = /<img +src *= *"([a-z0-9_]+\.[a-z]+)"/g
-          while (matches = re.exec(originExplain)) {
-            console.log(`matched2: ${matches[1]} and ${matches[1]}`)
-            files.push({filename: matches[1], originUri: matches[1]})
-          }
+    a.then(({explain, files}) => {
+      console.log('successfully     ' + explain)
+      let cefr = cefr_words[word]
+      let subTitle = subtitle_words[word]
+      dispatch(retrivedWordInfo({...subTitle, 
+        cefr, 
+        explain,
+        userComments: '',
+        review,
+        files
+      }))
+    }).catch(e => {console.log(e)})
+    console.log('my test here ')
 
-          return Promise.all(files.map(file => mdd.lookup(file.filename)))
-        }).then(raws => {
-          raws.forEach((raw, index) => {
-            let file = files[index]
-            let type = ''
-            let suffix = file.filename.substr(file.filename.length - 4)
-            switch(suffix) {
-              case '.spx':
-                let data = String.fromCharCode.apply(null, raw)
-                file.buffer = decodeFile(data)
-                console.log('buffer:     ', file.buffer )
-                type = 'audio/wav'
-                break
-              case '.mp3':
-                file.buffer = raw
-                type = 'audio/mp3'
-                break
-              case '.bmp':
-                file.buffer = raw
-                type = 'image/bmp'
-                break
-              case '.png':
-                file.buffer = raw
-                type = 'image/png'
-                break
-              case '.jpg':
-                file.buffer = raw
-                type = 'image/jpg'
-                break
-            }
-            let blob = new Blob([file.buffer], {type})
-            file.objectUri = URL.createObjectURL(blob)
-            originExplain = originExplain.replace(file.originUri, file.objectUri)
-          })
-          originExplain = originExplain.replace('<font color="silver">', '<font color="black">')
-          console.log('originExplain: ', originExplain)
-          let explain = originExplain
-          let cefr = cefr_words[word]
-          let subTitle = subtitle_words[word]
-          dispatch(retrivedWordInfo({...subTitle, 
-            cefr, 
-            explain,
-            userComments: '',
-            review,
-            files
-          }))
-        })
-      })
-    })
-  };
+  }
 }
